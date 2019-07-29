e6e955ea33e4bcc8f8edb88aea64891180957491
Eric <mcr.988899@163.com>
2019-07-29 14:24:03 +0800

    improve

--- script.js
+++ script.js
@@ -1,80 +1,104 @@
 window.onload=function(){
-var name = getCookie("username");
-alert("Name:"+name);
 
-	$("#sys_button").click(function(){
+  var username = getCookie("username");
+  if (username != "" && username != null){
+    $("#login").css("display","none");
+    $("#status_username").html(username);
+    $("#login_status").css("display","block");
+  }
+
+  var oldname = getCookie("oldname");
+  if (oldname != "" && oldname != null){
+    $("#input_id").val(oldname);
+  }
+
+  var oldpw = getCookie("oldpw");
+  if (oldpw != "" && oldpw != null){
+    $("#input_password").val(oldpw);
+  }
+
+  if (getCookie("remember_status") == "true"){
+    $("#rememberpw").prop("checked",true);
+  }
+
+  $("#sys_button").click(function(){
 		//调整两按钮样式
-	    $("#sys_button").attr("class","option_button_light");
-	    $("#member_button").attr("class","option_button");
-	    
+   $("#sys_button").attr("class","option_button_light");
+   $("#member_button").attr("class","option_button");
+
 	    //使实验室介绍显示
 	    $("#sys_intro").animate({height:'392px'});
     })
 
-    $("#member_button").click(function(){
+  $("#member_button").click(function(){
     	//调整两按钮样式
-	    $("#member_button").attr("class","option_button_light");
-	    $("#sys_button").attr("class","option_button");
-        
+     $("#member_button").attr("class","option_button_light");
+     $("#sys_button").attr("class","option_button");
+
         //使实验室介绍隐藏
-	    $("#sys_intro").animate({height:'0px'});
-    })
+        $("#sys_intro").animate({height:'0px'});
+      })
 
-    $("#login").click(function(){
-    	$("#login_box").slideToggle("slow");
-    	$("#id_warn").css("display","none");
-    	$("#pw_warn").css("display","none");
-    	$("#login_warn").css("display","none");
-    })
+  $("#login").click(function(){
+   $("#login_box").slideToggle("slow");
+   $("#id_warn").css("display","none");
+   $("#pw_warn").css("display","none");
+   $("#login_warn").css("display","none");
+ })
 
-    $("#close_box").click(function(){
-    	$("#login_box").slideUp("slow");
-    	$("#id_warn").css("display","none");
-    	$("#pw_warn").css("display","none");
-    	$("#login_warn").css("display","none");
-    })
+  $("#sign_out").click(function(){
+    setCookie("username",getCookie("username"),-1);
+    location.reload();
+  })
 
-    $("#submit").click(function(){
-    	$("#id_warn").css("display","none");
-    	$("#pw_warn").css("display","none");
-    	$("#login_warn").css("display","none");
-        var userName = $("#input_id").val();
-        var password = $("#input_password").val();
-        if (checkIfNull(userName,password)){
-           $("#wait_box").css("display","block");
-           changeDot();
-           checkPassword(userName,password);
-        }
-    })
+  $("#close_box").click(function(){
+   $("#login_box").slideUp("slow");
+   $("#id_warn").css("display","none");
+   $("#pw_warn").css("display","none");
+   $("#login_warn").css("display","none");
+ })
 
-    $("#register").click(function(){
-      window.open("https://github.com/join");
-    })
+  $("#submit").click(function(){
+   $("#id_warn").css("display","none");
+   $("#pw_warn").css("display","none");
+   $("#login_warn").css("display","none");
+   var userName = $("#input_id").val();
+   var password = $("#input_password").val();
+   if (checkIfNull(userName,password)){
+     $("#wait_box").css("display","block");
+     changeDot();
+     checkPassword(userName,password);
+   }
+ })
+
+  $("#register").click(function(){
+    window.open("https://github.com/join");
+  })
 }
 
 //动态改变登录提示框中的省略号个数
 function changeDot(){
-    var s = $("#dot").text();
-    if (s != "..."){
-      s = s+".";
-    }else{
-      s = "";
-    }
-    $("#dot").text(s);
-    setTimeout("changeDot()",500);
+  var s = $("#dot").text();
+  if (s != "..."){
+    s = s+".";
+  }else{
+    s = "";
+  }
+  $("#dot").text(s);
+  setTimeout("changeDot()",500);
 }
 
 //检查输入账号和密码是否为空,返回true-正确、false-存在空
 function checkIfNull(userName,password){
-    if (userName.length == 0){
-    	$("#id_warn").css("display","block");
-    	return false;
-    }else if (password.length == 0){
-    	$("#pw_warn").css("display","block");
-    	return false;
-    }else{
-    	return true;
-    }
+  if (userName.length == 0){
+   $("#id_warn").css("display","block");
+   return false;
+ }else if (password.length == 0){
+   $("#pw_warn").css("display","block");
+   return false;
+ }else{
+   return true;
+ }
 }
 
 //检查用户名密码是否正确
@@ -89,26 +113,33 @@ function checkPassword(userName,password){
           //登录成功
           success: function (data) {
             setCookie("username",userName,3);
-            
+            setCookie("oldname",userName,30);
+            if ($("#rememberpw").prop("checked")){
+              setCookie("oldpw",password,7);
+              setCookie("remember_status","true",7);
+            }else{
+              setCookie("oldpw",password,-1);
+              setCookie("remember_status","false",7);
+            }
             $("#wait_box").css("display","none");
-          	$("#login_box").css("display","none");
-          	$("#login").css("display","none");
-          	$("#status_username").html(userName);
-          	$("#login_status").css("display","block");
+            $("#login_box").css("display","none");
+            $("#login").css("display","none");
+            $("#status_username").html(userName);
+            $("#login_status").css("display","block");
           },
 
           //登录失败
           error: function (err) {
             $("#wait_box").css("display","none");
-          	$("#login_warn").css("display","block");
+            $("#login_warn").css("display","block");
           }
 
-      })
+        })
 }
 
 //设置用户登录信息
 function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
+  var d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
   var expires = "expires=" + d.toGMTString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
@@ -116,29 +147,17 @@ function setCookie(cname, cvalue, exdays) {
 
 //获取cookie信息
 function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-         }
-         if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-         }
-     }
-    return "";
-} 
-
-function checkCookie() {
-  var user=getCookie("username");
-  if (user != "") {
-    alert("再次欢迎您，" + user);
-  } else {
-     user = prompt("请输入姓名：","");
-     if (user != "" && user != null) {
-       setCookie("username", user, 30);
-     }
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
   }
+  return "";
 }
