33b5410cdbdbf490380278e8ed681cfb9804bd21
Eric <mcr.988899@163.com>
2019-08-25 15:02:58 +0800

    n

--- post-creator/post_creator.js
+++ post-creator/post_creator.js
@@ -1,35 +1,42 @@
 window.onload = function () {
-    $("#submit1").click(function(){
-      var files = document.getElementById('file_selector').files;
-      if (files.length > 0){
-        var file = files[0];
-        console.log(file.name);
-        console.log(file.size);
-        console.log(file.lastModifiedDate);
-        var reader = new FileReader();
-        reader.readAsText(file);
-        reader.onloadstart = function(){
-            $("#wait_box").css("display","block");
-        }
-        reader.onload = function(){
-        	creatFile(file.name,reader.result);
-        }
-      }
-    })
+	var username = getCookie("username");
+	var isLogined = (username != "" && username != null);
+	$("#submit1").click(function(){
+		if (!isLogined){
+			alert("请先登录！");
+		}else{
+			var files = document.getElementById('file_selector').files;
+			if (files.length > 0){
+				var file = files[0];
+				var reader = new FileReader();
+				reader.readAsText(file);
+				reader.onloadstart = function(){
+					$("#wait_box").css("display","block");
+				}
+				reader.onload = function(){
+					creatFile(file.name,reader.result);
+				}
+			}
+		}
+	})
 
-    $("#submit2").click(function(){
-    	var title = $("#title").val();
-    	var content = $("#content").val();
-    	console.log(title);
-    	creatFile(title+".md",content);
-    })
+	$("#submit2").click(function(){
+		if (!isLogined){
+			alert("请先登录！");
+		}else{
+			var title = $("#title").val();
+			var content = $("#content").val();
+			console.log(title);
+			creatFile(title+".md",content);
+		}
+	})
 
-    changeDot();
+	changeDot();
 };
 
 function creatFile(file_name,file_content){
 	var token = window.btoa("EricMCR" + ":" + "Ma1Chao2Ran3");
-	file_content = "---\nlayout: default\ntitle: "+file_name.split(".")[0]+"\n---\n" + file_content;
+	file_content = "---\nlayout: default\ntitle: "+file_name.split(".")[0]+"\nauthor: "+username+"\n---\n" + file_content;
 	var content = window.btoa(unescape(encodeURIComponent( file_content)));
 	var now = new Date();
 	var name = now.getFullYear()+"-"+("0"+(now.getMonth()+1)).slice(-2)+"-"+("0"+now.getDate()).slice(-2)+"-"+file_name;
@@ -58,14 +65,31 @@ function creatFile(file_name,file_content){
 	})
 }
 
+//获取cookie信息
+function getCookie(cname) {
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for(var i = 0; i <ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
 //动态改变登录提示框中的省略号个数
 function changeDot(){
-  var s = $("#dot").text();
-  if (s != "..."){
-    s = s+".";
-  }else{
-    s = "";
-  }
-  $("#dot").text(s);
-  setTimeout("changeDot()",500);
+	var s = $("#dot").text();
+	if (s != "..."){
+		s = s+".";
+	}else{
+		s = "";
+	}
+	$("#dot").text(s);
+	setTimeout("changeDot()",500);
 }
