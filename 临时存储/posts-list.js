359d9db120e21cf9e5e6be16a811c0de2e985450
Eric <mcr.988899@163.com>
2019-10-19 21:00:52 +0800

    d

--- 临时存储/posts-list.js
+++
@@ -1,9 +0,0 @@
-window.onload=function(){
-    $("#right p").html(getCategory());
-}
-
-function getCategory(){
-  var url = document.location.toString();//获取url地址
-  var urlParmStr = url.slice(url.indexOf('?')+1);//获取问号后所有的字符串
-  return urlParmStr;
-}
